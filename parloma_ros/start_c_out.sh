e989c3a1bbea1c4ff954196e6a98741545031d48
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-26 14:12:02 +0200

    Update start_c_out.sh

--- parloma_ros/start_c_out.sh
+++ parloma_ros/start_c_out.sh
@@ -2,7 +2,7 @@
 source ./devel/setup.bash
 sed -i "s/indirizzoipserver/"$IP"/g" ../Client/client_out/client.conf
 chmod +rx start_rviz.sh /Client/client_out/start_client.sh &&
-cd /Client/certificati/client_out && sync && ./start_client.sh &
+cd /Client/client_out && sync && ./start_client.sh &
 sleep 3s
 export ROS_IP=$(/sbin/ifconfig | grep "inet addr:10.8" |awk -F: '{print $2}' | awk '{print $1}')
 export ROS_MASTER_URI=http://10.8.0.1:11311
