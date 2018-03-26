1a511685946bd220b0d164015c65687546e9ae65
creos92 <33228553+creos92@users.noreply.github.com>
2018-03-26 14:11:36 +0200

    Update start_c_in.sh

--- parloma_ros/start_c_in.sh
+++ parloma_ros/start_c_in.sh
@@ -2,7 +2,7 @@
 source ./devel/setup.bash
 sed -i "s/indirizzoipserver/"$IP"/g" ../Client/client_in/client.conf
 chmod +rx start_play.sh /Client/client_in/start_client.sh &&
-cd /Client/certificati/client_in && sync && ./start_client.sh &
+cd /Client/client_in && sync && ./start_client.sh &
 sleep 3s
 export ROS_IP=$(/sbin/ifconfig | grep "inet addr:10.8" |awk -F: '{print $2}' | awk '{print $1}')
 export ROS_MASTER_URI=http://10.8.0.1:11311
